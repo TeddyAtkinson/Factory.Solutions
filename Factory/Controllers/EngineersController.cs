ef372bb7339d2d6306bbbc357a83876ed843043b
teddyatkinson <tedddy.atkinson@gmail.com>
2023-01-06 12:01:12 -0800

    Engineer creation and view page

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+  public class EngineerController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineerController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    // Routes
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      return View();
+    }
+  }
+}
